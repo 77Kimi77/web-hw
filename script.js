a17b4fd1bbe131a0127f0936a543d977a8e80617
77Kimi77 <rodionforgames@gmail.com>
2025-06-11 20:55:24 +0300

    Добавлена форма для персональных данных

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function submitForm() {
-    var firstname = document.getElementById("firstname").value;
-    var middlename = document.getElementById("middlename").value;
-    var lastname = document.getElementById("lastname").value;
-    var age = document.getElementById("age").value;
+    let firstname = document.getElementById("firstname").value;
+    let middlename = document.getElementById("middlename").value;
+    let lastname = document.getElementById("lastname").value;
+    let dateOfBirth = document.getElementById("dateOfBirth").value;
+    let age = 0;
 
-    if (firstname === "" || lastname === "" || age === "") {
+    if (firstname === "" || lastname === "") {
         alert("Заполните все поля!");
     } else {
-        alert("Данные успешно отправлены! Имя: " + firstname + " " + middlename + " " + lastname + ", Возраст: " + age + " лет.");
+        let birthDate = new Date(dateOfBirth);
+        let today = new Date();
+        age = today.getFullYear() - birthDate.getFullYear();
     }
 
     let output = document.getElementById("output");
@@ -16,4 +19,4 @@ function submitForm() {
         <p>Имя: ${firstname} ${middlename} ${lastname}</p>
         <p>Возраст: ${age} лет</p>
         `
-}
+    }
